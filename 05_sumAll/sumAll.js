23cf5e3fff8bdf58bb7995fb697e071d14aa9611
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-04-12 16:24:08 +0100

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function (...args) {
+  for (const arg of args) {
+    if (typeof arg !== "number" || arg < 0) {
+      return "ERROR";
+    }
+  }
 
+  args.sort((a, b) => a - b);
+  sum = 0;
+  for (let i = args[0]; i <= args[1]; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
