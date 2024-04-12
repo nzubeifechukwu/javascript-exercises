a13cde1f3d557cd363c19f334ab29f1f8a191d9b
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-04-12 12:43:26 +0100

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  newArray = [];
+  array.find((element) => {
+    if (!args.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
