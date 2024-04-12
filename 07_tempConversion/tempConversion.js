41964f335c80bd72c30ffe0eee45f659155fdb30
Nzube Ifechukwu <nzube.ifechukwu@gmail.com>
2024-04-12 17:24:08 +0100

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  return round((temperature - 32) * (5 / 9), 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  return round(temperature * (9 / 5) + 32, 1);
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
